fixed MOSES mesh strain vector computation

Motion_Analysis/mesh_statistics_tools.py
@@ -685,11 +685,12 @@ def vector_order_curve(vector_field, remove_mean=False):
     order_curve = []
 
     for frame in range(n_frames):
-        vectors = vector_field[:,frame]
+        vectors = vector_field[frame,:]
         order = vector_order(vectors, remove_mean=remove_mean)
         order_curve.append(order)
     return np.hstack(order_curve)
     
+
 def compute_mesh_order(mesh_strain_vector, remove_mean=False):
 
     """

run_single_video_example.py
@@ -269,10 +269,14 @@ if __name__=="__main__":
     mesh_order_curve_r = compute_mesh_order(mesh_strain_vector_r, remove_mean=False)
     mesh_order_curve_g = compute_mesh_order(mesh_strain_vector_g, remove_mean=False)
     
+    print 'mean mesh order red: %.3f' %(np.nanmean(mesh_order_curve_r))
+    print 'mean mesh order green: %.3f' %(np.nanmean(mesh_order_curve_g))
+    
+    
     plt.figure()
     plt.title('Mesh Order')
-    plt.plot(mesh_order_curve_r, label='Red')
-    plt.plot(mesh_order_curve_g, label='Green')
+    plt.plot(mesh_order_curve_r, 'r', label='Red')
+    plt.plot(mesh_order_curve_g, 'g', label='Green')
     plt.legend(loc='best')
     plt.xlabel('Frame Number')
     plt.ylabel('Mesh Order')