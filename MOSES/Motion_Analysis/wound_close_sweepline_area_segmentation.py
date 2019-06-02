47303c375d8a0d93638cfa6d84b71c11c2d71ba5
Felix <felixzhou1@gmail.com>
2019-06-02 17:29:46 +0100

    fix xrange to range

--- MOSES/Motion_Analysis/wound_close_sweepline_area_segmentation.py
+++ MOSES/Motion_Analysis/wound_close_sweepline_area_segmentation.py
@@ -125,7 +125,7 @@ def baseline_als(y, lam, p, niter=10):
     D = sparse.csc_matrix(np.diff(np.eye(L), 2))
     w = np.ones(L)
 
-    for i in xrange(niter):
+    for i in range(niter):
         W = sparse.spdiags(w, 0, L, L)
         Z = W + lam * D.dot(D.transpose())
         z = spsolve(Z, w*y)
